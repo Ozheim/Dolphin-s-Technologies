1b03bd947c027563943520d4aa2d7ddb38d6e7cf
Ozheim <heikel.zouaoui@epitech.eu>
2024-10-09 12:17:18 +0200

    header and navigation

--- frontend/src/Component/Header.jsx
+++ frontend/src/Component/Header.jsx
@@ -1,19 +1,21 @@
 import Logo from "../assets/logo.png";
 import "../Styles/Components/Header.scss";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 const Header = () =>{
 
     return(
+        <div className="header-container">
         <div className="header">
             <div>
                 <img src={Logo} alt="logo" className="logo"/>
             </div>
             <ul>
-                <li><a href="../Pages/Login.jsx">Mon compte</a></li>
-                <li><a href="../Pages/accèsRecruteur">Accès Recruteur</a></li>
-                <li><a href="../Pages/emploi">Emploi</a></li>
-                <li><a href=""></a></li>
+                <li><a href="../HeadHunter" className="header-list">Accès recruteur</a></li>
+                <li><a href="../emploi" className="header-list">Emploi</a></li>
+                <li><a href="../Login.jsx"className="connexion-link"><FontAwesomeIcon icon={faUser} />Connexion</a></li>    
             </ul>
+        </div>
         </div>
     )
 }
