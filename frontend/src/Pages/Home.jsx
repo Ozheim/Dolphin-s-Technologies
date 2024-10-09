fb8eac0d4d0882eeeb3fd2844f6529cfbb0a18a1
Ozheim <heikel.zouaoui@gmail.com>
2024-10-09 15:36:20 +0200

    :sparkle: footer functionnal

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,13 +1,19 @@
 import Header from "../Component/Header.jsx";
+import Navigation from "../Component/Navigation.jsx";
+import Footer from "../Component/Footer.jsx";
 const Home = () => {
 
 
     return (
         <div>
-            <Header/>
-                      
+            <Header/>   
+            <Navigation/>
+            <Footer/>
+   
+
         </div>
     );
 }
 
 export default Home;
+
