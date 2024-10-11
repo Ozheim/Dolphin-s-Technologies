02f513fdb04361149a93d967db5e43e8232b9cbe
lencedev <owen1.bolling@epitech.eu>
2024-10-11 15:06:07 +0200

    :bug:

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createUser, login } from '../controllers/userController.js';
+import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.post('/users', createUser);
-router.post('/login', login);
+router.post('/login', auth, login);
 
 router.get('/protected-route', (req, res) => {
     res.json({ message: 'Cette route est protégée et vous êtes authentifié.' });
