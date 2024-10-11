6e98f16a8edbd80d456ff616fa297b91e879c866
Ozheim <heikel.zouaoui@gmail.com>
2024-10-11 12:16:37 +0200

    :sparkles effect wave done

--- frontend/src/utils/FooterTransition.jsx
+++ frontend/src/utils/FooterTransition.jsx
@@ -1,14 +1,15 @@
-import { useNavigate } from 'react-router-dom';
 import "../Styles/Components/FooterTransition.scss";
-import { wind } from 'fontawesome';
+
 
 function FooterTransition (path) {
+
         const oceanElement = document.querySelector('.ocean');
         oceanElement.classList.add('lol');
         if (oceanElement) {
             setTimeout(() => {
                 window.location.pathname = "/" + path
-            }, 2500);
+            }, 800);
         }
 }
 export default FooterTransition;
+
