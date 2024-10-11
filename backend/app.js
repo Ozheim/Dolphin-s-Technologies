a115b8b365aeb6ad13b73509b50003319ce393d8
lencedev <owen1.bolling@epitech.eu>
2024-10-11 14:20:52 +0200

    :cave: code

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -19,27 +18,4 @@ app.use(express.json());
 app.use('/api', userRoutes);
 app.use("/api", jobRoutes);
 
-=======
-
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js';
-
-
-dotenv.config();
-const app = express();
-
-mongoose.connect(process.env.MONGODB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log("Connexion à MongoDB réussie !"))
-    .catch((error) => console.log("Connexion à MongoDB échouée....", error));
-
-    app.use(express.json());
-    app.use('/api', userRoutes);
-
-
->>>>>>> ad8685839590d2dc38de74985ed3492225b57584
 export default app;
