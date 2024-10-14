e1939b4bf7818a66365194797ca47267dc7e8228
Ozheim <heikel.zouaoui@gmail.com>
2024-10-14 10:56:31 +0200

    Wave, header, and footer style in red implemented for Headhunter

--- frontend/src/Pages/HeadHunter.jsx
+++ frontend/src/Pages/HeadHunter.jsx
@@ -1,22 +1,28 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import classNames from 'classnames'; 
 import FooterTransitionDown from "../utils/FooterTransitonDown";
 import Footer from "../Component/Footer";
 import Header from "../Component/Header";
 import "../Styles/Components/FooterTransition.scss";
-import React, { useEffect } from 'react';
+import "../Styles/Pages/HeadHunter.scss";
 
+const HeadHunter = () => {
+    const location = useLocation();
 
-const HeadHunter = () =>{
     useEffect(() => {
         FooterTransitionDown();
     }, []);
 
-       return(
-    <div>
-        <Header/>
-        <Footer/>
-    </div>
-    )   
-}
+    const isHeadhunterPage = location.pathname === '/HeadHunter';
 
+    return (
+      <div>
+        <Header className={classNames({ 'header-red': isHeadhunterPage, 'header': !isHeadhunterPage })} />
+    
+        <Footer className={classNames({ 'ocean-red': isHeadhunterPage, 'ocean': !isHeadhunterPage })} isHeadhunterPage={isHeadhunterPage} />
+      </div>
+    );
+};
 
 export default HeadHunter;
