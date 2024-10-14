17db6f9d94facb8d957ab8fded39cfe5ff19c5fe
Ozheim <heikel.zouaoui@gmail.com>
2024-10-14 11:58:27 +0200

    GoogleAPi auto-complete implemented with style

--- frontend/src/Pages/HeadHunter.jsx
+++ frontend/src/Pages/HeadHunter.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import classNames from 'classnames'; 
 import FooterTransitionDown from "../utils/FooterTransitonDown";
@@ -9,6 +9,8 @@ import "../Styles/Pages/HeadHunter.scss";
 
 const HeadHunter = () => {
     const location = useLocation();
+     const [email, setemail] = useState();
+     const [password, setpassword] = useState();
 
     useEffect(() => {
         FooterTransitionDown();
@@ -19,7 +21,26 @@ const HeadHunter = () => {
     return (
       <div>
         <Header className={classNames({ 'header-red': isHeadhunterPage, 'header': !isHeadhunterPage })} />
-    
+        <div>
+        <div className="container">
+        <form action="">
+        <h1>Connexion</h1>
+        <div className="name">
+          <label for="userName">Nom d'utilisateur</label>
+          <input type="text" id="userName" name="userName" onChange={(e)=> setemail(e.target.value)} />
+        </div>
+        <div className="password">
+          <label for="userPassword">Mot de passe</label>
+          <input type="password" id="userPassword" name="userPassword" onChange={(e)=> setpassword(e.target.value)} />
+        </div>
+        <button type="submit">GO !</button>
+        <p>
+          Vous n'avez pas encore de compte ? <a href="signin">Inscrivez-vous</a>
+        </p>
+        </form>
+      </div>
+
+    </div>
         <Footer className={classNames({ 'ocean-red': isHeadhunterPage, 'ocean': !isHeadhunterPage })} isHeadhunterPage={isHeadhunterPage} />
       </div>
     );
