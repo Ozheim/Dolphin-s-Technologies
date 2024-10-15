4952b701a6ba7ace22e11b2f4b3afa1d9136b44b
lencedev <owen1.bolling@epitech.eu>
2024-10-15 11:30:00 +0200

    commit risqué

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import Header from "../Component/Header.jsx"
 import Navigation from "../Component/Navigation.jsx";
 import Footer from "../Component/Footer.jsx";
 import JobsList from "../Component/JobsList.jsx";
+
 // import Fetch from "../Component/Fetch.jsx";
 
 
