2104f2a982792cf3cc51e1974e5d5bd626961be3
Vince <vvaugrente@gmail.com>
2024-10-18 14:57:33 +0200

    dashboard

--- frontend/src/Pages/HeadHunterDashboard.jsx
+++ frontend/src/Pages/HeadHunterDashboard.jsx
@@ -8,30 +8,6 @@ const HeadHunterDashBoard = () => {
     const [hunter, setHunter] = useState([]);
 
     useEffect(() => {
-        const fetchHunter = async () => {
-            try {
-                const token = localStorage.getItem("huntertoken");
-                const id = localStorage.getItem("hunterId"); 
-                
-                const reshunter = await axios.get(`http://localhost:5000/api/headhunters/${id}`, {
-                    headers: {
-                        "Authorization": `Bearer ${token}` 
-                    }
-                });
-
-                console.log('API Response:', reshunter.data);
-
-                if (reshunter.data && reshunter.data.length) {
-                    setHunter(reshunter.data);
-                } else {
-                    console.log("No hunter found.");
-                }
-            } catch (error) {
-                console.log("Error fetching hunters:", error.response ? error.response.data.message : error.message);
-            }
-        };
-        
-        fetchHunter();
     }, []);
 
     return (
