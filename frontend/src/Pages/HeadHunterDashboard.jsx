ff8c01a158466e095189c05b6525522b2540b218
Ozheim <heikel.zouaoui@gmail.com>
2024-10-19 16:00:48 +0200

    HeadHunterDashboard upddated

--- frontend/src/Pages/HeadHunterDashboard.jsx
+++ frontend/src/Pages/HeadHunterDashboard.jsx
@@ -1,27 +1,18 @@
 import React, { useEffect, useState, useContext } from "react";
 import Header from "../Component/Header";
 import Footer from "../Component/Footer";
-<<<<<<< HEAD
+
 import axios from "axios"
 import { AuthContext } from "../utils/AuthContext.jsx";
 import "../Styles/Pages/HeadHunterDashBoard.scss";
-
-const HeadHunterDashBoard = () => {
-  const { companyName } = useContext(AuthContext);
-  const { hunterId } = useContext(AuthContext);
-  console.log(companyName);
-  const [jobsById, setJobsById] = useState([]);
-=======
 import classNames from 'classnames';
-import "../Styles/Pages/HeadHunterDashBoard.scss";
 
 const HeadHunterDashBoard = () => {
     const [hunter, setHunter] = useState([]);
     const companyName = localStorage.getItem('companyName');
     const role = localStorage.getItem('role');
->>>>>>> 7eca5097e18336a25f13acf212830625ef811879
 
-  const id = hunterId
+
 
   useEffect(() => {
       const fetchJobsbyID = async () => {
@@ -34,33 +25,8 @@ const HeadHunterDashBoard = () => {
       };
       fetchJobsbyID();
     }, []);
-<<<<<<< HEAD
-  return (
-    <>
-      <Header />
-      <div className="dashboard-container">
-        <div className="informations-card">
-          <div>
-            <div className="hunter_container">
-              <h2>{companyName}</h2>
-            </div>
-          </div>
-        </div>
-        <div className="annonces">
-          <h1>Your Posted Jobs</h1>
-          <div className="offercards">
-            <h2>Title</h2>
-            <p>Location</p>
-            <p>Description</p>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </>
-);
-=======
 
-    return (
+      return (
         <>
             <Header companyName={companyName} role={role} className={classNames({ 'header-red': 1 })} />
             <div className="dashboard-container">
@@ -82,6 +48,5 @@ const HeadHunterDashBoard = () => {
 
         </>
     );
->>>>>>> 7eca5097e18336a25f13acf212830625ef811879
 };
 export default HeadHunterDashBoard;
