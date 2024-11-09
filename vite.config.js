2d26496a2df25d9625ec4b7f314bb0e23712d267
Ozheim <heikel.zouaoui@epitech.eu>
2024-11-09 19:05:47 +0100

    d*

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // S'assurer que la sortie est vers le bon répertoire
+  },
+});
