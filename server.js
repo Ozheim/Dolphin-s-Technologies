d7391dcf2fe954d46de31b4fb8d81ae97a3f03c4
Ozheim <heikel.zouaoui@epitech.eu>
2024-11-11 16:29:35 +0100

    changed arbo

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import app from "./app.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import path from "path";
+import express from "express";
+import path from "path";
 
 dotenv.config();
 
